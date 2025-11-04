0a9328117df0bb497cfcd66cdffc66686ab305cd
pankaj <pkp20022@gmail.com>
2025-11-04 22:55:24 +0530

    added initial files

---
+++ Contribute.md
@@ -0,0 +1,17 @@
+## Manual Installation
+ - Inatall nodejs locally
+ - clone the repo
+ - Install dependencies (npm install)
+ - Start the DB locally
+    - docker run -e POSTGRES_PASSWORD=mysecretpassword -d -p 5432:5432 postgres
+    - or Go to neon.tech and get yourself a new DB
+ - Change the .env file and update your DB credentials
+ - npx prisma migrate
+ - npx prisma generate
+ - npm run build 
+ - npm run start
+
+## Docker installation
+
+
+## Docker Compose installtion steps
---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express from "express"
+import { PrismaClient } from "./generated/prisma/client.js"
+
+const app = express()
+const prismaClient = new PrismaClient();
+
+// @ts-ignore
+app.get("/", async (req, res) => {
+
+    const data = await prismaClient.user.findMany();
+    
+    res.json({
+        data
+    })
+})
+
+// @ts-ignore
+app.post("/", async (req, res) => {
+    await prismaClient.user.create({
+        data:{
+            username: Math.random().toString(),
+            password: Math.random().toString()
+        }
+    })
+    res.json({
+        "message": "Post Endpoint"
+    })
+})
+
+app.listen(3000)
